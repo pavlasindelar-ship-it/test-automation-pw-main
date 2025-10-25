a89f17a9175d1dd4329670df6e2d01ddd63fbf47
pavlasindelar-ship-it <pavla.sindelar@gmail.com>
2025-10-25 12:00:19 +0200

    update

--- src/tests/example.spec.js
+++ src/tests/example.spec.js
@@ -1,4 +1,4 @@
-import { test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 test("should open login page", async ({ page }) => {
     await page.goto("https://team8-2022brno.herokuapp.com/prihlaseni");
@@ -46,6 +46,11 @@ test("tlacitko", async ({ page }) => {
     console.log("Logged user: " + await userName.textContent());
 });
 
+test("lesson 4", async ({ page }) => {
+    await page.goto("https://team8-2022brno.herokuapp.com/prihlaseni");
+    await expect(page).toHaveScreenshot();
+
+});
 
 
 
--- src/tests/homework/02/homework.spec.js
+++ src/tests/homework/02/homework.spec.js
@@ -1,6 +1,34 @@
 import { test, expect } from '@playwright/test';
  
-test("homework", async ({ page }) => {
+test("homework 1", async ({ page }) => {
+    await page.goto("https://team8-2022brno.herokuapp.com/registrace");
+    const aktualniCas = Date.now();
+    await page.locator("input#name").fill("Pavla Šindelářová");
+    await page.locator("input#email").fill("pavla.sindelar" + aktualniCas + "@gmail.com");
+    await page.locator("input#password").fill("Pavla12345");
+    await page.locator("input#password-confirm").fill("Pavla12345");
+    await page.locator(".btn-primary").click();
+    await expect(page).toHaveURL("https://team8-2022brno.herokuapp.com/zaci");
+});
 
-    console.log("Good job!");
+test("homework 2", async ({ page }) => {
+    await page.goto("https://team8-2022brno.herokuapp.com/registrace");
+    let aktualniCas = Date.now();
+    await page.locator("input#name").fill("Pavla Šindelářová");
+    await page.locator("input#email").fill("pavla.sindelar@gmail.com");
+    await page.locator("input#password").fill("Pavla12345");
+    await page.locator("input#password-confirm").fill("Pavla12345");
+    await page.locator(".btn-primary").click();
+    await expect(page.locator("text=Účet s tímto emailem již existuje")).toBeVisible();
+});
+
+test("homework 3", async ({ page }) => {
+    await page.goto("https://team8-2022brno.herokuapp.com/registrace");
+    const aktualniCas = Date.now();
+    await page.locator("input#name").fill("Pavla Šindelářová");
+    await page.locator("input#email").fill("pavla.sindelar" + aktualniCas + "@gmail.com");
+    await page.locator("input#password").fill("12345");
+    await page.locator("input#password-confirm").fill("12345");
+    await page.locator(".btn-primary").click();
+    await expect(page.locator("text=Heslo musí obsahovat minimálně 6 znaků, velké i malé písmeno a číslici")).toBeVisible();
 });

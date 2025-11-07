3568e5b9d5d0853ca7cafc359a56a6d730745413
pavlasindelar-ship-it <pavla.sindelar@gmail.com>
2025-11-07 18:21:28 +0100

    7.11.

--- src/tests/homework/02/homework.spec.js
+++ src/tests/homework/02/homework.spec.js
@@ -1,38 +1,25 @@
 require("dotenv").config();
 import { test, expect } from '@playwright/test';
 
-const userUsername = process.env.USER_USERNAME
-const userEmail = process.env.USER_EMAIL
-const userPassword = process.env.USER_PASSWORD
-const invalidPassword = process.env.INVALID_USER_PASSWORD
-const emailWithoutDomain = process.env.EMAIL_WITHOUT_DOMAIN
-const emailDomain = process.env.EMAIL_DOMAIN
-const aktualniCas = Date.now();
+const {
+    USER_USERNAME,
+    USER_EMAIL,
+    USER_PASSWORD,
+    INVALID_USER_PASSWORD,
+    EMAIL_WITHOUT_DOMAIN,
+    EMAIL_DOMAIN,
+}= process.env
 
-function getUserNameLocator(page) {
-    return page.locator("input#name");
+async function fillRegistrationForm(page, name, email, userPassword, confirmPassword) {
+    await page.locator("input#name").fill(name);
+    await page.locator("input#email").fill(email);
+    await page.locator("input#password").fill(userPassword);
+    await page.locator("input#password-confirm").fill(confirmPassword);
+    await page.locator(".btn-primary").click();
 }
 
-function getEmailLocator(page) {
-    return page.locator("input#email");
-}
-
-function getPasswordLocator(page) {
-    return page.locator("input#password");
-}
-
-function getPasswordConfirmationLocator(page) {
-    return page.locator("input#password-confirm");
-}
-
-function getButtonClickLocator(page) {
-    return page.locator(".btn-primary");
-}
-
-async function fillRegistrationForm(page, userUsername, userPassword) {
-    await getUserNameLocator(page).fill(userUsername);
-    await getPasswordLocator(page).fill(userPassword);
-    await getPasswordConfirmationLocator(page).fill(userPassword);
+function createNewEmail() {
+    return EMAIL_WITHOUT_DOMAIN + Date.now() + EMAIL_DOMAIN;
 }
 
 test.describe("login page", () => {
@@ -41,24 +28,24 @@ test.describe("login page", () => {
     }); 
 
     test("valid registration", { tag: ["@happyway", "@valid"] }, async ({ page }) => {
-        await fillRegistrationForm(page, userUsername, userPassword);
-        await getEmailLocator(page).fill(emailWithoutDomain + aktualniCas + emailDomain);
-        await getButtonClickLocator(page).click();
+        await fillRegistrationForm(page, USER_USERNAME, createNewEmail(), USER_PASSWORD, USER_PASSWORD);
         await expect(page).toHaveURL("https://team8-2022brno.herokuapp.com/zaci");
     });
 
     test("invalid registration with existing mail", { tag: "@invalid" }, async ({ page }) => {
-        await fillRegistrationForm(page, userUsername, userPassword);
-        await getEmailLocator(page).fill(userEmail);
-        await getButtonClickLocator(page).click();
+        await fillRegistrationForm(page, USER_USERNAME, USER_EMAIL, USER_PASSWORD, USER_PASSWORD);
         await expect(page.locator("text=Účet s tímto emailem již existuje")).toBeVisible();
     });
 
     test("invalid registration with incorrect password", { tag: "@invalid" }, async ({ page }) => {
-        await fillRegistrationForm(page, userUsername, invalidPassword);
-        await getEmailLocator(page).fill(emailWithoutDomain + aktualniCas + 1 + emailDomain);
-        await getButtonClickLocator(page).click();
+        await fillRegistrationForm(page, USER_USERNAME, createNewEmail(), INVALID_USER_PASSWORD, INVALID_USER_PASSWORD);
         await expect(page.locator("text=Heslo musí obsahovat minimálně 6 znaků, velké i malé písmeno a číslici")).toBeVisible();
     });
+    
+    test("invalid registration with incorrect confirmation", { tag: "@invalid" }, async ({ page }) => {
+        await fillRegistrationForm(page, USER_USERNAME, createNewEmail(), USER_PASSWORD, INVALID_USER_PASSWORD);
+        await expect(page.locator("text=Hesla se neshodují")).toBeVisible();
+    });
+
 });
  
